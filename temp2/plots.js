7aa62d9af83abd5c01b737eff30511370ae85143
David Phipps <elminestrone@gmail.com>
2020-08-20 16:32:30 -0500

    2/3 working

--- temp2/plots.js
+++ temp2/plots.js
@@ -33,5 +33,41 @@ function buildMetadata(sample) {
     });
 }
 
+function buildCharts(sample) {
+    d3.json("samples.json").then((data) => {
+        // Extract sample data
+        var samples = data.samples;
+        var sampleArray = samples.filter(sampleObj => sampleObj.id == sample);
+        var result = sampleArray[0];
 
+        // Trace for the sample data. Selecting only last 10 and reversing so it looks
+        // nice in the bar chart
+        var trace = {
+            x: result.sample_values.slice(0, 10).reverse(),
+            y: result.otu_ids.slice(0, 10).map(x => "OTU " + x).reverse(),
+            text: result.otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h"
+        };
+
+        // Setting our layout object and encapsulating our trace object in an array 
+        var data = [trace];
+        // Apply the group bar mode to the layout
+        var layout = {
+            title: "Search results",
+            font: {
+                family: 'Raleway, sans-serif'
+            },
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 100
+            }
+        };
+
+        // Render the plot to the div tag with id "plot"
+        Plotly.newPlot("plot", data, layout);
+    })
+}
 init();
