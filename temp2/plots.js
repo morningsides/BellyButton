1f58d76cedf0d57c603b832ffa90123271af927e
David Phipps <elminestrone@gmail.com>
2020-08-22 16:36:53 -0500

    working with default value

--- temp2/plots.js
+++ temp2/plots.js
@@ -15,7 +15,8 @@ function init() {
 
 function optionChanged(newSample) {
     buildMetadata(newSample);
-    buildCharts(newSample);
+    buildBar(newSample);
+    buildPlt(newSample);
 }
 
 function buildMetadata(sample) {
@@ -33,7 +34,7 @@ function buildMetadata(sample) {
     });
 }
 
-function buildCharts(sample) {
+function buildBar(sample) {
     d3.json("samples.json").then((data) => {
         // Extract sample data
         var samples = data.samples;
@@ -70,4 +71,43 @@ function buildCharts(sample) {
         Plotly.newPlot("plot", data, layout);
     })
 }
+
+function buildPlt(sample) {
+    d3.json("samples.json").then((data) => {
+        // Extract sample data
+        var samples = data.samples;
+        var sampleArray = samples.filter(sampleObj => sampleObj.id == sample);
+        var result = sampleArray[0];
+
+        // Trace for the sample data. Selecting only last 10 and reversing so it looks
+        // nice in the bar chart
+        var trace = {
+            x: result.otu_ids.slice(0, 10),
+            y: result.sample_values.slice(0, 10),
+            mode: 'markers',
+            marker: {
+                color: ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+                    '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D'
+                ],
+                opacity: [.5],
+                size: result.sample_values.slice(0, 10)
+            }
+        };
+        var data = [trace];
+
+        var layout = {
+            title: 'BellyBubble Chart',
+            showlegend: false,
+            height: 600,
+            width: 600
+        };
+        // Render the plot to the div tag with id "plot"
+        Plotly.newPlot('bubble', data, layout);
+    })
+}
+
+
 init();
+buildMetadata('940');
+buildBar('940');
+buildPlt('940');
